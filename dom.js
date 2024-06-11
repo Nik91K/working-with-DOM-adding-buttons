0bcc6d63f6fc15efb59002c5b1154327cd20ba23
Nik91K <nikotakovsl@gmail.com>
2024-06-11 13:42:35 +0300

    modal window

--- dom.js
+++ dom.js
@@ -3,6 +3,23 @@ let data = await response.json();
 
 console.log(data);
 
+// модальне вікно
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+// .
+
 function createTracks(band)
 { 
   let values = "";
